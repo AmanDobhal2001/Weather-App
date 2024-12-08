02ee71821048d7bb4fbbde2d6ec83125bc0ff1f8
Aman Dobhal <dobhalaman4@gmail.com>
2024-12-08 19:11:26 +0530

    content is pushed

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,15 +1,82 @@
 import React from 'react'
-import Image from '/src/assets/image.jpg'
+import { useState } from 'react'
+import { LiaTemperatureHighSolid } from "react-icons/lia";
+import { TiWeatherWindyCloudy } from "react-icons/ti";
 
 const Content=()=> {
+
   return (
   <>
-    <div className="container flex flex-wrap w-full m-4">
-      <div  className="item w-1/3 min-h-[35vh] mr-3 rounded-2xl backdrop-blur-md border-2 border-slate-300 rounded-xl">
-    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti excepturi odit incidunt. Mollitia veniam cupiditate animi similique impedit delectus, consequuntur ex quos.
+    <div className="container flex flex-row flex-wrap justify-around w-10/12 mt-16">
+    <div className="item w-80 h-[35vh] p-3 font-serif flex flex-col items-center gap-3 text-white rounded-2xl backdrop-blur-md">
+        <div className='flex flex-row h-10  text-stone-300 '>
+        <h5>Temp </h5>
+        <LiaTemperatureHighSolid className='h-8 w-8'/>
+        </div>
+        <div className="min-w-56 h-0.5 bg-slate-300"/>
+        <div>Temperature is<span className='ml-1' id="temperature"></span></div>
+        <div>Sunrise :<span id="sunrise"></span></div>
+        <div>Sunset :<span id="sunset"></span></div>
       </div>
+
+      <div  className="item w-80 h-[35vh] p-3 font-serif items-center text-white gap-3 flex flex-col rounded-2xl backdrop-blur-md">
+      <div className='flex flex-row h-10 '>
+        <h5>Atmosphere </h5>
+        <TiWeatherWindyCloudy className='h-8 w-8'/>
+        </div>
+
+        <div className="min-w-56 h-0.5 bg-slate-300"/>
+        <div>The humidity is<span id="humidity"></span></div>
+        <div>Visibility :<span id="visibility"></span></div>
+        <div>Pressure :<span id="pressure"></span></div>
+      </div>
+
+      <div  className="item w-80 h-[35vh] p-3 font-serif items-center text-white gap-3 flex flex-col rounded-2xl backdrop-blur-md">
+        <h5>Forcast</h5>
+        <div className="min-w-56 h-0.5 bg-slate-300"/>
+        <div>The day is<span id="day"></span></div>
+        <div>Highest temp :<span id="highest"></span></div>
+        <div>Lowest temp :<span id="lowest"></span></div>
+      </div>  
     </div>
 
+
+
+    <div className='flex flex-row w-3/6 mt-16 ml-48 text-white h-40 rounded-2xl backdrop-blur-md'>
+      <div className='flex flex-col w-24 ml-20 mt-5 h-28 justify-between'>
+        <h6>10:00:00</h6>
+        <h6>14:00:00</h6>
+        <h6>17:00:00</h6>
+      </div>
+
+    <div className='flex flex-col w-full'>
+      <div className='flex flex-row w-full'>
+        <h5 className='ml-7'>Temperature</h5>
+        <h5 className='ml-20'>Humidity</h5>
+        <h5 className='ml-32'>Forcast</h5>
+      </div>
+
+    <div className='flex flex-row w-full mt-3 justify-around'>
+      <div className='flex flex-col h-28 justify-between'>
+        <p id="temp10"></p>
+        <p id="temp2"></p>
+        <p id="temp5"></p>
+      </div>
+
+      <div className='flex flex-col   h-28 justify-between'>
+        <p id="humidity10"></p>
+        <p id="humidity2"></p>
+        <p id="humidity5"></p>
+      </div>
+
+      <div className='flex flex-col h-28 justify-between'>
+        <p id="forcast10"></p>
+        <p id="forcast2"></p>
+        <p id="forcast5"></p>
+      </div>
+    </div>
+    </div>
+    </div>
   </>
   );
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,72 @@
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { TbCloudSearch } from "react-icons/tb";
+import { useEffect, useState } from 'react'
+import React from 'react'
+import Content from './Content';
 
 function NavScrollExample() {
+
+  
+  const [place, setplace] = useState("delhi")
+
+  const handleChange=(e)=>{
+    setplace(e.target.value)
+    console.log(place)
+  }
+
+  useEffect(() => {
+    return () => {
+      getData();
+    }
+  }, [])
+  
+
+  const  getData=async()=> {
+
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}?unitGroup=us&key=HHFTWYRNKS4VYZFY63FETDKHL&contentType=json`;
+    const options = {
+        method: 'GET',
+        headers: {
+
+        }
+    };
+    
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+
+        temperature.innerHTML=Math.floor((result.days[0].temp-32)*(5/9))+" °C";
+        sunrise.innerHTML=" "+result.days[0].sunrise;
+        sunset.innerHTML=" "+result.days[0].sunset;
+        humidity.innerHTML=" "+result.days[0].humidity+" %";
+        visibility.innerHTML=" "+result.days[0].visibility+" Km";
+        pressure.innerHTML=" "+result.days[0].pressure+" mb";
+        day.innerHTML=" "+result.days[0].conditions;
+        highest.innerHTML=" "+Math.floor((result.days[0].tempmax-32)*(5/9))+" °C";
+        lowest.innerHTML=" "+Math.floor((result.days[0].tempmin-32)*(5/9))+" °C";
+
+        temp10.innerHTML=Math.floor((result.days[0].hours[11].temp-32)*(5/9))+" °C";
+        temp2.innerHTML=Math.floor((result.days[0].hours[14].temp-32)*(5/9))+" °C";
+        temp5.innerHTML=Math.floor((result.days[0].hours[17].temp-32)*(5/9))+" °C";
+
+        humidity10.innerHTML=result.days[0].hours[11].humidity;
+        humidity2.innerHTML=result.days[0].hours[14].humidity;;
+        humidity5.innerHTML=result.days[0].hours[17].humidity;;
+
+        forcast10.innerHTML=result.days[0].hours[11].conditions;
+        forcast2.innerHTML=result.days[0].hours[14].conditions;;
+        forcast5.innerHTML=result.days[0].hours[17].conditions;;
+
+    } catch (error) {
+        console.error(error);
+    }
+  }
+
   return (
-    <Navbar expand="lg" className="">
+    <Navbar expand="lg" className="backdrop-blur-md">
       <Container fluid>
         <Navbar.Brand className="text-white" style={{fontWeight:'bold',marginLeft:'93px',fontSize:'25px'}}>Weathery !</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
@@ -20,13 +80,17 @@ function NavScrollExample() {
           <Form className="d-flex">
             <Form.Control
               type="search"
-              placeholder="Search"
-              className="me-2"
+              className="h-7"
               aria-label="Search"
+              border="2px"
+              border-radius="10px"
+              border-color="black"
+              value={place}
+              onChange={handleChange}
             />
-            <Button className="text-white" variant="">Search</Button>
           </Form>
         </Navbar.Collapse>
+        <button onClick={getData} className='w-9 h-7 mr-10 ml-5'><TbCloudSearch className='w-8 h-6 text-white active:h-4'/></button>
       </Container>
     </Navbar>
   );
